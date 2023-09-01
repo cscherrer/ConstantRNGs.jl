93a2ff3e10945c8237b3fd8636f64c590b037b84
Documenter.jl <documenter@juliadocs.github.io>
2023-09-01 18:15:23 +0000

    build based on 5bb6649

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FixedRNGs","category":"page"},{"location":"#FixedRNGs","page":"Home","title":"FixedRNGs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FixedRNGs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FixedRNGs]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConstantRNGs","category":"page"},{"location":"#ConstantRNGs","page":"Home","title":"ConstantRNGs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConstantRNGs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConstantRNGs]","category":"page"}]
 }
